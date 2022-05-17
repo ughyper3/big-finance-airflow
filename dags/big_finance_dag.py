f11ffe3a0052717f0ba44357f416a0e2fde52bdb
ughyper3 <ugodimini@gmail.com>
2022-05-17 20:17:35 +0200

    setup mongo db connection and quotation push

--- dags/big_finance_dag.py
+++ dags/big_finance_dag.py
@@ -2,15 +2,37 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
+from pymongo import MongoClient
 from requests import get
 
 
-def fetch_quotation_from_big_finance(**kwargs):
-    company = kwargs['company']
+def init_mongo_connection():
+
+    URI = f'mongodb+srv://airflow:adminpassword@cluster0.jdvey.mongodb.net/test'
+    try:
+        client = MongoClient(URI)
+        print(f'Successful connection to mongo db')
+        return client
+    except Exception as e:
+        print(f'Exception {e} during mongo db connection')
+
+
+def fetch_quotation_from_big_finance(company):
     header = {"Authorization": "Token 84170d390b7cdd924efeb95be75b8266cf15a50f"}
     response = get(f"https://bigfinance-api.herokuapp.com/finnhub/api/v1/quote/{company}/", headers=header).json()
     return response
+
+
+def push_quotation_on_datalake(**kwargs):
+    client = init_mongo_connection()
+    db = client.big_finance
+    company = kwargs['company']
+    quotation = fetch_quotation_from_big_finance(company)
+    try:
+        result = db.reviews.insert_one(quotation)
+    except Exception as e:
+        print(f'push_quotation_on_datalake exception : {e}')
+
 
 with DAG(
     'big_finance_dag',
@@ -38,10 +60,7 @@ with DAG(
     for company in companies:
         task = PythonOperator(
             task_id=f'fetch_quotation_from_{company}',
-            python_callable=fetch_quotation_from_big_finance,
+            python_callable=push_quotation_on_datalake,
             op_kwargs={'company': company},
             dag=dag
         )
-
-
-
